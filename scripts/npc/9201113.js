59b396644484ea7aaf4ad51f52859eea50c9dd6a
Policy <173628847+iampolicy@users.noreply.github.com>
2026-08-30 02:48:42 -0400

    qol: clarify Crimsonwood Keep party quest messages

--- scripts/npc/9201113.js
+++ scripts/npc/9201113.js
@@ -1,28 +1,7 @@
 /*
 	This file is part of the OdinMS Maple Story Server
-    Copyright (C) 2008 Patrick Huy <patrick.huy@frz.cc> 
-                       Matthias Butz <matze@odinms.de>
-                       Jan Christian Meyer <vimes@odinms.de>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License version 3
-    as published by the Free Software Foundation. You may not use, modify
-    or distribute this program under any other version of the
-    GNU Affero General Public License.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-/*Jack
- *
- *@author Alan (SharpAceX)
- *@author Ronan
- */
+/* Jack - Crimsonwood Keep Party Quest */
 
 var status = 0;
 var expedition;
@@ -31,150 +10,149 @@ var player;
 var em;
 const ExpeditionType = Java.type('server.expeditions.ExpeditionType');
 var cwkpq = ExpeditionType.CWKPQ;
-var list = "What would you like to do?#b\r\n\r\n#L1#View current Expedition members#l\r\n#L2#Start the fight!#l\r\n#L3#Stop the expedition.#l";
+var list = "What would you like to do?#b\r\n\r\n#L1#View current Expedition members#l\r\n#L2#Start the party quest!#l\r\n#L3#Stop the expedition.#l";
 
 function start() {
     action(1, 0, 0);
 }
 
 function action(mode, type, selection) {
-
     player = cm.getPlayer();
     expedition = cm.getExpedition(cwkpq);
     em = cm.getEventManager("CWKPQ");
 
-    if (mode == -1) {
+    if (mode == -1 || mode == 0) {
         cm.dispose();
-    } else {
-        if (mode == 0) {
+        return;
+    }
+
+    if (status == 0) {
+        if (player.getLevel() < cwkpq.getMinLevel() || player.getLevel() > cwkpq.getMaxLevel()) {
+            cm.sendOk("You cannot enter Crimsonwood Keep Party Quest at your current level.\r\n\r\nRequired: #bLv. " + cwkpq.getMinLevel() + " - " + cwkpq.getMaxLevel() + "#k\r\nYour level: #rLv. " + player.getLevel() + "#k");
             cm.dispose();
-            return;
-        }
-
-        if (status == 0) {
-            if (player.getLevel() < cwkpq.getMinLevel() || player.getLevel() > cwkpq.getMaxLevel()) { //Don't fit requirement, thanks Conrad
-                cm.sendOk("You do not meet the criteria to take attempt Crimsonwood Keep Party Quest!");
-                cm.dispose();
-            } else if (expedition == null) { //Start an expedition
-                cm.sendSimple("#e#b<Party Quest: Crimsonwood Keep>\r\n#k#n" + em.getProperty("party") + "\r\n\r\nWould you like to assemble a team to attempt the #rCrimsonwood Keep Party Quest#k?\r\n#b#L1#Lets get this going!#l\r\n\#L2#No, I think I'll wait a bit...#l");
-                status = 1;
-            } else if (expedition.isLeader(player)) { //If you're the leader, manage the exped
-                if (expedition.isInProgress()) {
-                    cm.sendOk("Your expedition is already in progress, for those who remain battling lets pray for those brave souls.");
-                    cm.dispose();
-                } else {
-                    cm.sendSimple(list);
-                    status = 2;
-                }
-            } else if (expedition.isRegistering()) { //If the expedition is registering
-                if (expedition.contains(player)) { //If you're in it but it hasn't started, be patient
-                    cm.sendOk("You have already registered for the expedition. Please wait for #r" + expedition.getLeader().getName() + "#k to begin it.");
-                    cm.dispose();
-                } else { //If you aren't in it, you're going to get added
-                    cm.sendOk(expedition.addMember(cm.getPlayer()));
-                    cm.dispose();
-                }
-            } else if (expedition.isInProgress()) { //Only if the expedition is in progress
-                if (expedition.contains(player)) { //If you're registered, warp you in
-                    em.getInstance("CWKPQ" + player.getClient().getChannel()).registerPlayer(player);
-                    cm.dispose();
-                } else { //If you're not in by now, tough luck
-                    cm.sendOk("Another expedition has taken the initiative to complete the Crimsonwood Keep Party Quest, lets pray for those brave souls.");
-                    cm.dispose();
-                }
-            }
-        } else if (status == 1) {
-            if (selection == 1) {
-                expedition = cm.getExpedition(cwkpq);
-                if (expedition != null) {
-                    cm.sendOk("Someone already taken the initiative to be the leader of the expedition. Try joining them!");
-                    cm.dispose();
-                    return;
-                }
-
-                var res = cm.createExpedition(cwkpq);
-                if (res == 0) {
-                    cm.sendOk("The #rCrimsonwood Keep Party Quest Expedition#k has been created.\r\n\r\nTalk to me again to view the current team, or start the fight!");
-                } else if (res > 0) {
-                    cm.sendOk("Sorry, you've already reached the quota of attempts for this expedition! Try again another day...");
-                } else {
-                    cm.sendOk("An unexpected error has occurred when starting the expedition, please try again later.");
-                }
-
-                cm.dispose();
-
-            } else if (selection == 2) {
-                cm.sendOk("Sure, not everyone's up to attempting Crimsonwood Keep Party Quest.");
-                cm.dispose();
-
-            }
-        } else if (status == 2) {
-            if (selection == 1) {
-                if (expedition == null) {
-                    cm.sendOk("The expedition could not be loaded.");
-                    cm.dispose();
-                    return;
-                }
-                expedMembers = expedition.getMemberList();
-                var size = expedMembers.size();
-                if (size == 1) {
-                    cm.sendOk("You are the only member of the expedition.");
-                    cm.dispose();
-                    return;
-                }
-                var text = "The following members make up your expedition (Click on them to expel them):\r\n";
-                text += "\r\n\t\t1." + expedition.getLeader().getName();
-                for (var i = 1; i < size; i++) {
-                    text += "\r\n#b#L" + (i + 1) + "#" + (i + 1) + ". " + expedMembers.get(i).getValue() + "#l\n";
-                }
-                cm.sendSimple(text);
-                status = 6;
-            } else if (selection == 2) {
-                var min = cwkpq.getMinSize();
-                var size = expedition.getMemberList().size();
-                if (size < min) {
-                    cm.sendOk("You need at least " + min + " players registered in your expedition.");
-                    cm.dispose();
-                    return;
-                }
-
-                cm.sendOk("The expedition will begin and you will now be escorted to the #bEntrance to CWKPQ Altar#k.");
-                status = 4;
-            } else if (selection == 3) {
-                const PacketCreator = Java.type('tools.PacketCreator');
-                player.getMap().broadcastMessage(PacketCreator.serverNotice(6, expedition.getLeader().getName() + " has ended the expedition."));
-                cm.endExpedition(expedition);
-                cm.sendOk("The expedition has now ended. Sometimes the best strategy is to run away.");
-                cm.dispose();
-
-            }
-        } else if (status == 4) {
-            if (em == null) {
-                cm.sendOk("The event could not be initialized, please report this on the forum.");
-                cm.dispose();
-                return;
-            }
-
-            em.setProperty("leader", player.getName());
-            em.setProperty("channel", player.getClient().getChannel());
-            if (!em.startInstance(expedition)) {
-                cm.sendOk("Another expedition has taken the initiative to complete the Crimsonwood Keep Party Quest, lets pray for those brave souls.");
-                cm.dispose();
-                return;
-            }
-
+        } else if (em == null) {
+            cm.sendOk("Crimsonwood Keep Party Quest is temporarily unavailable. Please report this in EverLeaf's bug-report channel.");
             cm.dispose();
-
-        } else if (status == 6) {
-            if (selection > 0) {
-                var banned = expedMembers.get(selection - 1);
-                expedition.ban(banned);
-                cm.sendOk("You have banned " + banned.getValue() + " from the expedition.");
+        } else if (expedition == null) {
+            cm.sendSimple("#e#b<Party Quest: Crimsonwood Keep>\r\n#k#n" + em.getProperty("party") + "\r\n\r\nWould you like to assemble a team?\r\n#b#L1#Create the expedition.#l\r\n#L2#Not yet.#l");
+            status = 1;
+        } else if (expedition.isLeader(player)) {
+            if (expedition.isInProgress()) {
+                cm.sendOk("Your Crimsonwood Keep expedition is already in progress in this channel.");
                 cm.dispose();
             } else {
                 cm.sendSimple(list);
                 status = 2;
             }
+        } else if (expedition.isRegistering()) {
+            if (expedition.contains(player)) {
+                cm.sendOk("You are already registered. Expedition leader: #b" + expedition.getLeader().getName() + "#k. Wait for the leader to begin the party quest.");
+                cm.dispose();
+            } else {
+                cm.sendOk(expedition.addMember(player));
+                cm.dispose();
+            }
+        } else if (expedition.isInProgress()) {
+            if (expedition.contains(player)) {
+                var eim = em.getInstance("CWKPQ" + player.getClient().getChannel());
+                if (eim == null) {
+                    cm.sendOk("The active Crimsonwood Keep instance could not be loaded. Please report this in EverLeaf's bug-report channel.");
+                } else {
+                    eim.registerPlayer(player);
+                }
+                cm.dispose();
+            } else {
+                cm.sendOk("Another expedition is already running Crimsonwood Keep Party Quest in this channel. Try another channel or wait for the current run to finish.");
+                cm.dispose();
+            }
+        }
+    } else if (status == 1) {
+        if (selection == 1) {
+            expedition = cm.getExpedition(cwkpq);
+            if (expedition != null) {
+                cm.sendOk("An expedition is already being organized in this channel. You can join that group instead.");
+                cm.dispose();
+                return;
+            }
+
+            var res = cm.createExpedition(cwkpq);
+            if (res == 0) {
+                cm.sendOk("The #rCrimsonwood Keep Party Quest Expedition#k has been created. Talk to me again when your team is ready.");
+            } else if (res > 0) {
+                cm.sendOk("You have reached the entry-attempt limit for Crimsonwood Keep Party Quest. Try again after the attempt limit resets.");
+            } else {
+                cm.sendOk("The expedition could not be created because of an unexpected server error. Please try again, and report it if the problem continues.");
+            }
+            cm.dispose();
+        } else if (selection == 2) {
+            cm.dispose();
+        }
+    } else if (status == 2) {
+        if (selection == 1) {
+            if (expedition == null) {
+                cm.sendOk("The expedition could not be loaded. Please talk to me again.");
+                cm.dispose();
+                return;
+            }
+
+            expedMembers = expedition.getMemberList();
+            var size = expedMembers.size();
+            if (size == 1) {
+                cm.sendOk("You are currently the only registered member.");
+                cm.dispose();
+                return;
+            }
+
+            var text = "Registered expedition members (select a member to remove them):\r\n";
+            text += "\r\n\t\t1. " + expedition.getLeader().getName();
+            for (var i = 1; i < size; i++) {
+                text += "\r\n#b#L" + (i + 1) + "#" + (i + 1) + ". " + expedMembers.get(i).getValue() + "#l\n";
+            }
+            cm.sendSimple(text);
+            status = 6;
+        } else if (selection == 2) {
+            var min = cwkpq.getMinSize();
+            var size = expedition.getMemberList().size();
+            if (size < min) {
+                cm.sendOk("Your expedition does not have enough registered players.\r\n\r\nRequired: #b" + min + "#k\r\nCurrently registered: #r" + size + "#k");
+                cm.dispose();
+                return;
+            }
+
+            cm.sendOk("Your expedition is ready. You will now be escorted to the #bCrimsonwood Keep altar entrance#k.");
+            status = 4;
+        } else if (selection == 3) {
+            const PacketCreator = Java.type('tools.PacketCreator');
+            player.getMap().broadcastMessage(PacketCreator.serverNotice(6, expedition.getLeader().getName() + " has ended the expedition."));
+            cm.endExpedition(expedition);
+            cm.sendOk("The expedition has been ended.");
+            cm.dispose();
+        }
+    } else if (status == 4) {
+        if (em == null) {
+            cm.sendOk("Crimsonwood Keep Party Quest could not be initialized. Please report this in EverLeaf's bug-report channel.");
+            cm.dispose();
+            return;
+        }
+
+        em.setProperty("leader", player.getName());
+        em.setProperty("channel", player.getClient().getChannel());
+        if (!em.startInstance(expedition)) {
+            cm.sendOk("A Crimsonwood Keep Party Quest run is already active in this channel. Try another channel or wait for the current run to finish.");
+            cm.dispose();
+            return;
+        }
+
+        cm.dispose();
+    } else if (status == 6) {
+        if (selection > 0) {
+            var banned = expedMembers.get(selection - 1);
+            expedition.ban(banned);
+            cm.sendOk(banned.getValue() + " has been removed from the expedition.");
+            cm.dispose();
+        } else {
+            cm.sendSimple(list);
+            status = 2;
         }
     }
 }
