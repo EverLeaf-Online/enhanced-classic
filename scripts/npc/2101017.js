5a193b553d53a2a393edff04defee6ee3d50f8cf
Policy <173628847+iampolicy@users.noreply.github.com>
2026-08-30 02:49:36 -0400

    qol: clarify Ariant Coliseum battle messages

--- scripts/npc/2101017.js
+++ scripts/npc/2101017.js
@@ -1,15 +1,9 @@
 /*2101017.js
- *Cesar
- *@author Jvlaple
+ * Cesar
+ * @author Jvlaple
  */
 
 var status = 0;
-var toBan = -1;
-var choice;
-var arena;
-var arenaName;
-var type;
-var map;
 const ExpeditionType = Java.type('server.expeditions.ExpeditionType');
 var exped;
 var expedicao;
@@ -20,130 +14,133 @@ function start() {
 }
 
 function action(mode, type, selection) {
-
-    if (mode == -1) {
+    if (mode == -1 || mode == 0) {
         cm.dispose();
-    } else {
-        if (mode == 0) {
+        return;
+    }
+
+    const GameConstants = Java.type('constants.game.GameConstants');
+    var mapId = cm.getPlayer().getMapId();
+
+    if (mapId == 980010100 || mapId == 980010200 || mapId == 980010300) {
+        if (mapId == 980010100) {
+            exped = ExpeditionType.ARIANT;
+        } else if (mapId == 980010200) {
+            exped = ExpeditionType.ARIANT1;
+        } else {
+            exped = ExpeditionType.ARIANT2;
+        }
+
+        expedicao = cm.getExpedition(exped);
+        if (expedicao == null) {
+            cm.sendOk("This Ariant Coliseum lobby is no longer active. Return to the main lobby and choose another arena.");
             cm.dispose();
             return;
         }
 
-        const GameConstants = Java.type('constants.game.GameConstants');
-        if (cm.getPlayer().getMapId() == 980010100 || cm.getPlayer().getMapId() == 980010200 || cm.getPlayer().getMapId() == 980010300) {
-            if (cm.getPlayer().getMapId() == 980010100) {
-                exped = ExpeditionType.ARIANT;
-                expedicao = cm.getExpedition(exped);
-
-            } else if (cm.getPlayer().getMapId() == 980010200) {
-                exped = ExpeditionType.ARIANT1;
-                expedicao = cm.getExpedition(exped);
+        expedMembers = expedicao.getMemberList();
+        if (status == 0) {
+            if (cm.isLeaderExpedition(exped)) {
+                cm.sendSimple("What would you like to do?#b\r\n#L1#View current members#l\r\n#L2#Remove a member#l\r\n#L3#Start the battle#l\r\n#L4#Leave and close the arena#l");
+                status = 1;
             } else {
-                exped = ExpeditionType.ARIANT2;
-                expedicao = cm.getExpedition(exped);
-            }
-
-            if (expedicao == null) {
+                cm.sendOk("Current members inside this arena:\r\n#b" + cm.getExpeditionMemberNames(exped));
                 cm.dispose();
-                return;
             }
+        } else if (status == 1) {
+            if (selection == 1) {
+                cm.sendOk("Current members inside this arena:\r\n#b" + cm.getExpeditionMemberNames(exped));
+                cm.dispose();
+            } else if (selection == 2) {
+                var size = expedMembers.size();
+                if (size == 1) {
+                    cm.sendOk("You are currently the only player registered in this arena.");
+                    cm.dispose();
+                    return;
+                }
 
-            expedMembers = expedicao.getMemberList();
-            if (status == 0) {
-                if (cm.isLeaderExpedition(exped)) {
-                    cm.sendSimple("What would you like to do? #b\r\n#L1#View current members#l\r\n#L2#Ban members#l\r\n#L3#Start the battle#l\r\n#L4#Leave the arena#l");
-                    status = 1;
+                var text = "Select a member to remove from the arena:\r\n";
+                text += "\r\n\t\t1. " + expedicao.getLeader().getName();
+                for (var i = 1; i < size; i++) {
+                    text += "\r\n#b#L" + (i + 1) + "#" + (i + 1) + ". " + expedMembers.get(i).getValue() + "#l\n";
+                }
+                cm.sendSimple(text);
+                status = 6;
+            } else if (selection == 3) {
+                var memberCount = expedicao.getMembers().size();
+                if (memberCount < 2) {
+                    cm.sendOk("At least #b2 players#k are required to start an Ariant Coliseum battle.\r\n\r\nCurrently registered: #r" + memberCount + "#k");
+                    cm.dispose();
+                    return;
+                }
+                if (cm.getParty() != null) {
+                    cm.sendOk("Leave your regular party before starting Ariant Coliseum. The arena uses its own participant group.");
+                    cm.dispose();
+                    return;
+                }
+
+                var errorMsg = cm.startAriantBattle(exped, mapId);
+                if (errorMsg != "") {
+                    cm.sendOk("The battle could not start:\r\n\r\n" + errorMsg);
+                }
+                cm.dispose();
+            } else if (selection == 4) {
+                cm.mapMessage(5, "The arena leader has closed the lobby.");
+                expedicao.warpExpeditionTeam(980010000);
+                cm.endExpedition(expedicao);
+                cm.dispose();
+            }
+        } else if (status == 6) {
+            if (selection > 0 && selection <= expedMembers.size()) {
+                var banned = expedMembers.get(selection - 1);
+                if (banned.getValue() == expedicao.getLeader().getName()) {
+                    cm.sendOk("The arena leader cannot remove themselves here. Use 'Leave and close the arena' instead.");
                 } else {
-                    var toSend = "Current members inside this arena:\r\n#b";
-                    toSend += cm.getExpeditionMemberNames(exped);
-                    cm.sendOk(toSend);
-                    cm.dispose();
-                }
-            } else if (status == 1) {
-                if (selection == 1) {
-                    var toSend = "Current members inside this arena:\r\n#b";
-                    toSend += cm.getExpeditionMemberNames(exped);
-                    cm.sendOk(toSend);
-                    cm.dispose();
-                } else if (selection == 2) {
-                    var size = expedMembers.size();
-                    if (size == 1) {
-                        cm.sendOk("You are the only member of the expedition.");
-                        cm.dispose();
-                        return;
-                    }
-                    var text = "The following members make up your expedition (Click on them to expel them):\r\n";
-                    text += "\r\n\t\t1." + expedicao.getLeader().getName();
-                    for (var i = 1; i < size; i++) {
-                        text += "\r\n#b#L" + (i + 1) + "#" + (i + 1) + ". " + expedMembers.get(i).getValue() + "#l\n";
-                    }
-                    cm.sendSimple(text);
-                    status = 6;
-                } else if (selection == 3) {
-                    if (expedicao.getMembers().size() < 1) {
-                        cm.sendOk("Need one more players to start the battle.");
-                        cm.dispose();
-                    } else {
-                        if (cm.getParty() != null) {
-                            cm.sendOk("You cannot enter the battle as a party group.");
-                            cm.dispose();
-                            return;
-                        }
-
-                        var errorMsg = cm.startAriantBattle(exped, cm.getPlayer().getMapId());
-                        if (errorMsg != "") {
-                            cm.sendOk(errorMsg);
-                        }
-
-                        cm.dispose();
-                    }
-                } else if (selection == 4) {
-                    cm.mapMessage(5, "The Leader of the arena left.");
-                    expedicao.warpExpeditionTeam(980010000);
-                    cm.endExpedition(expedicao);
-                    cm.dispose();
-                }
-            } else if (status == 6) {
-                if (selection > 0) {
-                    var banned = expedMembers.get(selection - 1);
                     expedicao.ban(banned);
-                    cm.sendOk("You have banned " + banned.getValue() + " from the expedition.");
-                    cm.dispose();
-                } else {
-                    cm.sendSimple(list);
-                    status = 2;
+                    cm.sendOk(banned.getValue() + " has been removed from the arena.");
                 }
-            }
-        } else if (GameConstants.isAriantColiseumArena(cm.getPlayer().getMapId())) {
-            if (cm.getPlayer().getMapId() == 980010101) {
-                exped = ExpeditionType.ARIANT;
-                expedicao = cm.getExpedition(exped);
-            } else if (cm.getPlayer().getMapId() == 980010201) {
-                exped = ExpeditionType.ARIANT1;
-                expedicao = cm.getExpedition(exped);
+                cm.dispose();
             } else {
-                exped = ExpeditionType.ARIANT2;
-                expedicao = cm.getExpedition(exped);
+                cm.sendOk("That member selection is no longer valid. Please talk to me again.");
+                cm.dispose();
             }
-            if (status == 0) {
-                var gotTheBombs = expedicao.getProperty("gotBomb" + cm.getChar().getId());
-                if (gotTheBombs != null) {
-                    cm.sendOk("I already gave you the bomb, please kill the #bScorpio#k now!");
-                    cm.dispose();
-                } else if (cm.canHoldAll([2270002, 2100067], [50, 5])) {
-                    cm.sendOk("I have given you (5) #b#eBombs#k#n and (50) #b#eElement Rock#k#n.\r\nUse the Elementary Rocks to capture the scorpions for #r#eSpirit Jewels#k#n!");
-                    expedicao.setProperty("gotBomb" + cm.getChar().getId(), "1");
-                    cm.gainItem(2270002, 50);
-                    cm.gainItem(2100067, 5);
-                    cm.dispose();
-                } else {
-                    cm.sendOk("It seems that your inventory is full.");
-                    cm.dispose();
-                }
-            }
+        }
+        return;
+    }
+
+    if (GameConstants.isAriantColiseumArena(mapId)) {
+        if (mapId == 980010101) {
+            exped = ExpeditionType.ARIANT;
+        } else if (mapId == 980010201) {
+            exped = ExpeditionType.ARIANT1;
         } else {
-            cm.sendOk("Hi there, have you heard of the Ariant Coliseum Battle Arena, it's a competitive event available to players between level 20 to 30!");
+            exped = ExpeditionType.ARIANT2;
+        }
+
+        expedicao = cm.getExpedition(exped);
+        if (expedicao == null) {
+            cm.sendOk("This Ariant Coliseum match is no longer active. If you appear stuck, use @unstuck.");
+            cm.dispose();
+            return;
+        }
+
+        if (status == 0) {
+            var gotTheBombs = expedicao.getProperty("gotBomb" + cm.getChar().getId());
+            if (gotTheBombs != null) {
+                cm.sendOk("You already received your arena supplies. Use the Element Rocks to capture Scorpios and collect Spirit Jewels.");
+            } else if (cm.canHoldAll([2270002, 2100067], [50, 5])) {
+                cm.sendOk("You received #b50 Element Rocks#k and #b5 Bombs#k.\r\n\r\nUse the Element Rocks to capture Scorpios for #rSpirit Jewels#k.");
+                expedicao.setProperty("gotBomb" + cm.getChar().getId(), "1");
+                cm.gainItem(2270002, 50);
+                cm.gainItem(2100067, 5);
+            } else {
+                cm.sendOk("You need enough free USE inventory space to receive #b50 Element Rocks#k and #b5 Bombs#k. Clear some space and talk to me again.");
+            }
             cm.dispose();
         }
+        return;
     }
+
+    cm.sendOk("Ariant Coliseum is a competitive arena for players from #bLv. 20 to Lv. 30#k. Enter through the Ariant Coliseum lobby to participate.");
+    cm.dispose();
 }
