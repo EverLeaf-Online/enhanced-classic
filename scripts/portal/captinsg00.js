10cec1a95d8e6eb4b8911ede346a245a4bd36824
Policy <173628847+iampolicy@users.noreply.github.com>
2026-08-30 02:40:26 -0400

    qol: clarify Captain Latanica entry failures

--- scripts/portal/captinsg00.js
+++ scripts/portal/captinsg00.js
@@ -2,31 +2,35 @@
 
 function enter(pi) {
     if (!pi.haveItem(4000381)) {
-        pi.playerMessage(5, "You do not have White Essence.");
+        pi.playerMessage(5, "You need a White Essence to challenge Captain Latanica.");
+        return false;
+    }
+
+    var em = pi.getEventManager("LatanicaBattle");
+    if (em == null) {
+        pi.playerMessage(5, "Captain Latanica is temporarily unavailable because the battle event could not be loaded. Please report this in EverLeaf's bug-report channel.");
+        return false;
+    }
+
+    if (pi.getParty() == null) {
+        pi.playerMessage(5, "You need a party to challenge Captain Latanica.");
+        return false;
+    } else if (!pi.isLeader()) {
+        pi.playerMessage(5, "Only your party leader can start the Captain Latanica battle. Have the leader enter the portal first.");
         return false;
     } else {
-        var em = pi.getEventManager("LatanicaBattle");
-
-        if (pi.getParty() == null) {
-            pi.playerMessage(5, "You are currently not in a party, create one to attempt the boss.");
-            return false;
-        } else if (!pi.isLeader()) {
-            pi.playerMessage(5, "Your party leader must enter the portal to start the battle.");
-            return false;
-        } else {
-            var eli = em.getEligibleParty(pi.getParty());
-            if (eli.size() > 0) {
-                if (!em.startInstance(pi.getParty(), pi.getPlayer().getMap(), 1)) {
-                    pi.playerMessage(5, "The battle against the boss has already begun, so you may not enter this place yet.");
-                    return false;
-                }
-            } else {  //this should never appear
-                pi.playerMessage(5, "You cannot start this battle yet, because either your party is not in the range size, some of your party members are not eligible to attempt it or they are not in this map. If you're having trouble finding party members, try Party Search.");
+        var eli = em.getEligibleParty(pi.getParty());
+        if (eli.size() > 0) {
+            if (!em.startInstance(pi.getParty(), pi.getPlayer().getMap(), 1)) {
+                pi.playerMessage(5, "Captain Latanica is already being challenged in this channel. Try another channel or wait for the current battle to finish.");
                 return false;
             }
-
-            pi.playPortalSound();
-            return true;
+        } else {
+            pi.playerMessage(5, "Your party is not currently eligible for Captain Latanica. Make sure the required party members are eligible and together in this map before the leader enters.");
+            return false;
         }
+
+        pi.playPortalSound();
+        return true;
     }
 }
