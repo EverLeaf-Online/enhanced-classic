9908d278ed7385b69fc1dd2abe1bee924b77c8ba
Policy <173628847+iampolicy@users.noreply.github.com>
2026-08-30 00:39:18 -0400

    fix: preserve gachapon tickets when inventory is full

--- scripts/npc/gachaponRemote.js
+++ scripts/npc/gachaponRemote.js
@@ -46,12 +46,7 @@ function action(mode, type, selection) {
             status--;
         }
         if (status == 0 && cm.haveItem(ticketId)) {
-            if (cm.canHold(1302000) && cm.canHold(2000000) && cm.canHold(3010001) && cm.canHold(4000000)) { // One free slot in every inventory.
-                cm.gainItem(ticketId, -1);
-                cm.doGachapon();
-            } else {
-                cm.sendOk("Please have at least one slot in your #rEQUIP, USE, SET-UP, #kand #rETC#k inventories free.");
-            }
+            cm.doGachapon(ticketId);
         } else {
             cm.dispose();
         }
