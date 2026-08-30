2782cdf21d8aeec707a006e27978b9c5927ac279
Policy <173628847+iampolicy@users.noreply.github.com>
2026-08-30 04:52:59 -0400

    qol: clarify Kerning PQ entry and exit messages

--- scripts/npc/9020000.js
+++ scripts/npc/9020000.js
@@ -51,18 +51,19 @@ function action(mode, type, selection) {
 
         if (status == 0) {
             if (state == 1) {
-                cm.sendYesNo("Do you wish to abandon this area?");
+                cm.sendYesNo("Leave the Kerning City Party Quest and return to Kerning City? Your current PQ progress will be abandoned.");
             } else {
                 em = cm.getEventManager("KerningPQ");
                 if (em == null) {
-                    cm.sendOk("The Kerning PQ has encountered an error.");
+                    cm.sendOk("The Kerning City Party Quest is temporarily unavailable because its event could not be loaded. Please report this in EverLeaf's bug-report channel.");
                     cm.dispose();
+                    return;
                 } else if (cm.isUsingOldPqNpcStyle()) {
                     action(1, 0, 0);
                     return;
                 }
 
-                cm.sendSimple("#e#b<Party Quest: 1st Accompaniment>\r\n#k#n" + em.getProperty("party") + "\r\n\r\nHow about you and your party members collectively beating a quest? Here you'll find obstacles and problems where you won't be able to beat it without great teamwork. If you want to try it, please tell the #bleader of your party#k to talk to me.#b\r\n#L0#I want to participate in the party quest.\r\n#L1#I would like to " + (cm.getPlayer().isRecvPartySearchInviteEnabled() ? "disable" : "enable") + " Party Search.\r\n#L2#I would like to hear more details.");
+                cm.sendSimple("#e#b<Party Quest: 1st Accompaniment>\r\n#k#n" + em.getProperty("party") + "\r\n\r\nWork with your party to clear a series of cooperative stages. Keep all eligible members together in Kerning City and have the #bparty leader#k speak with me when you're ready.#b\r\n#L0#Enter the Kerning City Party Quest.\r\n#L1#" + (cm.getPlayer().isRecvPartySearchInviteEnabled() ? "Disable" : "Enable") + " Party Search invitations.\r\n#L2#Tell me how the Party Quest works.");
             }
         } else if (status == 1) {
             if (state == 1) {
@@ -71,29 +72,29 @@ function action(mode, type, selection) {
             } else {
                 if (selection == 0) {
                     if (cm.getParty() == null) {
-                        cm.sendOk("You can participate in the party quest only if you are in a party.");
+                        cm.sendOk("You need to be in a party before you can enter the Kerning City Party Quest.");
                         cm.dispose();
                     } else if (!cm.isLeader()) {
-                        cm.sendOk("Your party leader must talk to me to start this party quest.");
+                        cm.sendOk("Only your party leader can start this Party Quest. Have the leader speak with me once everyone is ready.");
                         cm.dispose();
                     } else {
                         var eli = em.getEligibleParty(cm.getParty());
                         if (eli.size() > 0) {
                             if (!em.startInstance(cm.getParty(), cm.getPlayer().getMap(), 1)) {
-                                cm.sendOk("Another party has already entered the #rParty Quest#k in this channel. Please try another channel, or wait for the current party to finish.");
+                                cm.sendOk("Another party is already running the Kerning City Party Quest in this channel. Try another channel or wait for the current group to finish.");
                             }
                         } else {
-                            cm.sendOk("You cannot start this party quest yet, because either your party is not in the range size, some of your party members are not eligible to attempt it or they are not in this map. If you're having trouble finding party members, try Party Search.");
+                            cm.sendOk("Your party is not currently eligible to enter. Check the required party size and levels, and make sure every participating member is together in Kerning City. Party Search can help if you still need members.");
                         }
 
                         cm.dispose();
                     }
                 } else if (selection == 1) {
                     var psState = cm.getPlayer().toggleRecvPartySearchInvite();
-                    cm.sendOk("Your Party Search status is now: #b" + (psState ? "enabled" : "disabled") + "#k. Talk to me whenever you want to change it back.");
+                    cm.sendOk("Party Search invitations are now #b" + (psState ? "enabled" : "disabled") + "#k. Talk to me whenever you want to change this setting.");
                     cm.dispose();
                 } else {
-                    cm.sendOk("#e#b<Party Quest: 1st Accompaniment>#k#n\r\nYour party must pass through many obstacles and puzzles while traversing the sub-objectives of this Party Quest. Coordinate with your team in order to further advance and defeat the final boss and collect the dropped item in order to access the rewards and bonus stage.");
+                    cm.sendOk("#e#b<Party Quest: 1st Accompaniment>#k#n\r\nYour party must solve cooperative objectives, coordinate through several stages, defeat the final boss, and collect the required item to reach the reward and bonus areas. Stay together and follow the leader's calls.");
                     cm.dispose();
                 }
             }
