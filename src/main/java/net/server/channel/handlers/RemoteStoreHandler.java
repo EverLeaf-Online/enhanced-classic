12c834024a5ea5b564cbf32decb3dcf417b28f52
Policy <173628847+iampolicy@users.noreply.github.com>
2026-08-30 18:27:50 -0400

    fix: enforce restricted-map rules for remote merchants (#33)

--- src/main/java/net/server/channel/handlers/RemoteStoreHandler.java
+++ src/main/java/net/server/channel/handlers/RemoteStoreHandler.java
@@ -26,7 +26,9 @@ import client.Character;
 import client.Client;
 import net.AbstractPacketHandler;
 import net.packet.InPacket;
+import server.maps.FieldLimit;
 import server.maps.HiredMerchant;
+import server.maps.MiniDungeonInfo;
 import tools.PacketCreator;
 
 /**
@@ -36,18 +38,40 @@ public class RemoteStoreHandler extends AbstractPacketHandler {
     @Override
     public void handlePacket(InPacket p, Client c) {
         Character chr = c.getPlayer();
+        if (!canAccessRemoteMerchant(chr)) {
+            chr.dropMessage(1, "You cannot access your Merchant from this map or while another interaction is active.");
+            c.sendPacket(PacketCreator.enableActions());
+            return;
+        }
+
         HiredMerchant hm = getMerchant(c);
         if (hm != null && hm.isOwner(chr)) {
-            if (hm.getChannel() == chr.getClient().getChannel()) {
+            if (hm.getChannel() == c.getChannel()) {
                 hm.visitShop(chr);
             } else {
                 c.sendPacket(PacketCreator.remoteChannelChange((byte) (hm.getChannel() - 1)));
             }
             return;
-        } else {
-            chr.dropMessage(1, "You don't have a Merchant open.");
         }
+
+        chr.dropMessage(1, "You don't have a Merchant open.");
         c.sendPacket(PacketCreator.enableActions());
+    }
+
+    private static boolean canAccessRemoteMerchant(Character chr) {
+        if (!chr.isAlive() || chr.isChangingMaps() || chr.isBanned()) {
+            return false;
+        }
+        if (chr.getCashShop().isOpened()
+                || chr.getEventInstance() != null
+                || MiniDungeonInfo.isDungeonMap(chr.getMapId())
+                || FieldLimit.CANNOTMIGRATE.check(chr.getMap().getFieldLimit())) {
+            return false;
+        }
+        return chr.getTrade() == null
+                && chr.getShop() == null
+                && chr.getPlayerShop() == null
+                && chr.getHiredMerchant() == null;
     }
 
     private static HiredMerchant getMerchant(Client c) {
