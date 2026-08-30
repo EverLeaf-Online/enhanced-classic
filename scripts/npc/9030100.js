5cf1f7c93ceb5f20dd920190ac66c619ef5d2a8a
Policy <173628847+iampolicy@users.noreply.github.com>
2026-08-30 04:13:40 -0400

    Add Free Market utility hub

--- scripts/npc/9030100.js
+++ scripts/npc/9030100.js
@@ -1,25 +1,54 @@
 /*
-	This file is part of the OdinMS Maple Story Server
-    Copyright (C) 2008 Patrick Huy <patrick.huy@frz.cc>
-		       Matthias Butz <matze@odinms.de>
-		       Jan Christian Meyer <vimes@odinms.de>
+ * EverLeaf Free Market Utility Hub
+ * Uses the existing storage NPC in the Free Market entrance so players
+ * get useful services without adding extra map clutter.
+ */
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation version 3 as published by
-    the Free Software Foundation. You may not use, modify or distribute
-    this program under any other version of the GNU Affero General Public
-    License.
+var status = -1;
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 function start() {
-    cm.getPlayer().getStorage().sendStorage(cm.getClient(), 9030100);
+    action(1, 0, 0);
+}
+
+function action(mode, type, selection) {
+    if (mode != 1) {
+        cm.dispose();
+        return;
+    }
+
+    status++;
+
+    if (status == 0) {
+        cm.sendSimple(
+            "Welcome to the #bEverLeaf Free Market#k. What do you need?" +
+            "#b\r\n#L0#Storage" +
+            "\r\n#L1#General supplies" +
+            "\r\n#L2#Instant Travel"
+        );
+        return;
+    }
+
+    if (status == 1) {
+        if (selection == 0) {
+            cm.getPlayer().getStorage().sendStorage(cm.getClient(), 9030100);
+            cm.dispose();
+            return;
+        }
+
+        if (selection == 1) {
+            // Reuse an existing general-store inventory rather than creating
+            // a progression-bypassing remote shop.
+            cm.openShopNPC(2100002);
+            cm.dispose();
+            return;
+        }
+
+        if (selection == 2) {
+            cm.dispose();
+            cm.openNpc(9000020);
+            return;
+        }
+    }
+
     cm.dispose();
 }
