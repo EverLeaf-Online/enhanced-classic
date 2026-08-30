e8fd3229fcc278a5a42481b13bdc4f5176eb3567
Policy <173628847+iampolicy@users.noreply.github.com>
2026-08-30 04:32:58 -0400

    qol: add Free Market return service

--- scripts/npc/9030100.js
+++ scripts/npc/9030100.js
@@ -5,6 +5,7 @@
  */
 
 var status = -1;
+var FREE_MARKET = 910000000;
 
 function start() {
     action(1, 0, 0);
@@ -23,7 +24,8 @@ function action(mode, type, selection) {
             "Welcome to the #bEverLeaf Free Market#k. What do you need?" +
             "#b\r\n#L0#Storage" +
             "\r\n#L1#General supplies" +
-            "\r\n#L2#Instant Travel"
+            "\r\n#L2#Instant Travel" +
+            "\r\n#L3#Return to previous location"
         );
         return;
     }
@@ -48,7 +50,35 @@ function action(mode, type, selection) {
             cm.openNpc(9000020);
             return;
         }
+
+        if (selection == 3) {
+            returnToPreviousLocation();
+            return;
+        }
     }
 
+    cm.dispose();
+}
+
+function returnToPreviousLocation() {
+    var savedMap = cm.getPlayer().peekSavedLocation("FREE_MARKET");
+
+    if (savedMap == -1 || savedMap == FREE_MARKET) {
+        cm.sendOk(
+            "I don't have a previous Free Market return location saved for you.\r\n\r\n" +
+            "Use the #bTRADE#k button from a normal map to save your location before entering the Free Market."
+        );
+        cm.dispose();
+        return;
+    }
+
+    var returnMap = cm.getPlayer().getSavedLocation("FREE_MARKET");
+    if (returnMap == -1 || returnMap == FREE_MARKET) {
+        cm.sendOk("Your saved Free Market return location is no longer available. Please use #bInstant Travel#k or #b@unstuck#k if you need help getting back to a town.");
+        cm.dispose();
+        return;
+    }
+
+    cm.warp(returnMap);
     cm.dispose();
 }
