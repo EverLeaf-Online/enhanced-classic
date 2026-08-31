c1b505074ffaed0ec3398c4f5a5dfc28db70aed6
Policy <173628847+iampolicy@users.noreply.github.com>
2026-08-30 23:11:05 -0400

    security: serialize item reward claims per client

--- src/main/java/net/server/channel/handlers/ItemRewardHandler.java
+++ src/main/java/net/server/channel/handlers/ItemRewardHandler.java
@@ -47,39 +47,48 @@ public final class ItemRewardHandler extends AbstractPacketHandler {
         byte slot = (byte) p.readShort();
         int itemId = p.readInt(); // will load from xml I don't care.
 
-        Item it = c.getPlayer().getInventory(InventoryType.USE).getItem(slot);   // null check here thanks to Thora
-        if (it == null || it.getItemId() != itemId || c.getPlayer().getInventory(InventoryType.USE).countById(itemId) < 1) {
+        if (!c.tryacquireClient()) {
+            c.sendPacket(PacketCreator.enableActions());
             return;
         }
 
-        ItemInformationProvider ii = ItemInformationProvider.getInstance();
-        Pair<Integer, List<RewardItem>> rewards = ii.getItemReward(itemId);
-        for (RewardItem reward : rewards.getRight()) {
-            if (!InventoryManipulator.checkSpace(c, reward.itemid, reward.quantity, "")) {
-                c.sendPacket(PacketCreator.getShowInventoryFull());
-                break;
+        try {
+            Item it = c.getPlayer().getInventory(InventoryType.USE).getItem(slot);   // null check here thanks to Thora
+            if (it == null || it.getItemId() != itemId || c.getPlayer().getInventory(InventoryType.USE).countById(itemId) < 1) {
+                return;
             }
-            if (Randomizer.nextInt(rewards.getLeft()) < reward.prob) {//Is it even possible to get an item with prob 1?
-                if (ItemConstants.getInventoryType(reward.itemid) == InventoryType.EQUIP) {
-                    final Item item = ii.getEquipById(reward.itemid);
-                    if (reward.period != -1) {
-                        // TODO is this a bug, meant to be 60 * 60 * 1000?
-                        item.setExpiration(currentServerTime() + reward.period * 60 * 60 * 10);
+
+            ItemInformationProvider ii = ItemInformationProvider.getInstance();
+            Pair<Integer, List<RewardItem>> rewards = ii.getItemReward(itemId);
+            for (RewardItem reward : rewards.getRight()) {
+                if (!InventoryManipulator.checkSpace(c, reward.itemid, reward.quantity, "")) {
+                    c.sendPacket(PacketCreator.getShowInventoryFull());
+                    break;
+                }
+                if (Randomizer.nextInt(rewards.getLeft()) < reward.prob) {//Is it even possible to get an item with prob 1?
+                    if (ItemConstants.getInventoryType(reward.itemid) == InventoryType.EQUIP) {
+                        final Item item = ii.getEquipById(reward.itemid);
+                        if (reward.period != -1) {
+                            // TODO is this a bug, meant to be 60 * 60 * 1000?
+                            item.setExpiration(currentServerTime() + reward.period * 60 * 60 * 10);
+                        }
+                        InventoryManipulator.addFromDrop(c, item, false);
+                    } else {
+                        InventoryManipulator.addById(c, reward.itemid, reward.quantity, "", -1);
                     }
-                    InventoryManipulator.addFromDrop(c, item, false);
-                } else {
-                    InventoryManipulator.addById(c, reward.itemid, reward.quantity, "", -1);
+                    InventoryManipulator.removeById(c, InventoryType.USE, itemId, 1, false, false);
+                    if (reward.worldmsg != null) {
+                        String msg = reward.worldmsg;
+                        msg.replaceAll("/name", c.getPlayer().getName());
+                        msg.replaceAll("/item", ii.getName(reward.itemid));
+                        Server.getInstance().broadcastMessage(c.getWorld(), PacketCreator.serverNotice(6, msg));
+                    }
+                    break;
                 }
-                InventoryManipulator.removeById(c, InventoryType.USE, itemId, 1, false, false);
-                if (reward.worldmsg != null) {
-                    String msg = reward.worldmsg;
-                    msg.replaceAll("/name", c.getPlayer().getName());
-                    msg.replaceAll("/item", ii.getName(reward.itemid));
-                    Server.getInstance().broadcastMessage(c.getWorld(), PacketCreator.serverNotice(6, msg));
-                }
-                break;
             }
+            c.sendPacket(PacketCreator.enableActions());
+        } finally {
+            c.releaseClient();
         }
-        c.sendPacket(PacketCreator.enableActions());
     }
 }
