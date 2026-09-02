174ab8cc582492341be06ca595cc80ce771bc10f
Policy <173628847+iampolicy@users.noreply.github.com>
2026-09-02 12:40:42 -0400

    feat(qa): extend learned buff registry for batch two

--- src/main/java/soloMapling/ArtificialPlayer/BotAttackSystem/BotBuffConfig.java
+++ src/main/java/soloMapling/ArtificialPlayer/BotAttackSystem/BotBuffConfig.java
@@ -1,21 +1,29 @@
 package soloMapling.ArtificialPlayer.BotAttackSystem;
 
 import client.Job;
+import constants.skills.Aran;
 import constants.skills.Archer;
 import constants.skills.Assassin;
 import constants.skills.Bandit;
 import constants.skills.Bishop;
+import constants.skills.BlazeWizard;
 import constants.skills.Bowmaster;
+import constants.skills.Brawler;
+import constants.skills.Buccaneer;
 import constants.skills.ChiefBandit;
 import constants.skills.Cleric;
+import constants.skills.Corsair;
 import constants.skills.Crossbowman;
 import constants.skills.Crusader;
 import constants.skills.DarkKnight;
+import constants.skills.DawnWarrior;
 import constants.skills.DragonKnight;
+import constants.skills.Evan;
 import constants.skills.FPArchMage;
 import constants.skills.FPMage;
 import constants.skills.FPWizard;
 import constants.skills.Fighter;
+import constants.skills.Gunslinger;
 import constants.skills.Hermit;
 import constants.skills.Hero;
 import constants.skills.Hunter;
@@ -25,13 +33,16 @@ import constants.skills.ILWizard;
 import constants.skills.Magician;
 import constants.skills.Marksman;
 import constants.skills.NightLord;
+import constants.skills.NightWalker;
 import constants.skills.Page;
 import constants.skills.Paladin;
 import constants.skills.Priest;
 import constants.skills.Shadower;
 import constants.skills.Spearman;
+import constants.skills.ThunderBreaker;
 import constants.skills.Warrior;
 import constants.skills.WhiteKnight;
+import constants.skills.WindArcher;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -39,7 +50,7 @@ import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
-/** SoloMapling bot buff lineage registry used by combat rendering. */
+/** Learned self-buff registry used by the headless QA combat loop. */
 public final class BotBuffConfig {
     private static final Map<Job, int[]> BUFFS_BY_JOB = new EnumMap<>(Job.class);
 
@@ -78,6 +89,35 @@ public final class BotBuffConfig {
         put(Job.MARKSMAN, Marksman.MAPLE_WARRIOR, Marksman.SHARP_EYES);
         put(Job.NIGHTLORD, NightLord.MAPLE_WARRIOR, NightLord.SHADOW_STARS);
         put(Job.SHADOWER, Shadower.MAPLE_WARRIOR);
+
+        put(Job.BRAWLER, Brawler.KNUCKLER_BOOSTER);
+        put(Job.BUCCANEER, Buccaneer.MAPLE_WARRIOR, Buccaneer.SPEED_INFUSION);
+        put(Job.GUNSLINGER, Gunslinger.GUN_BOOSTER);
+        put(Job.CORSAIR, Corsair.MAPLE_WARRIOR, Corsair.SPEED_INFUSION);
+
+        put(Job.DAWNWARRIOR1, DawnWarrior.IRON_BODY);
+        put(Job.DAWNWARRIOR2, DawnWarrior.SWORD_BOOSTER, DawnWarrior.RAGE);
+        put(Job.DAWNWARRIOR3, DawnWarrior.COMBO, DawnWarrior.SOUL_CHARGE);
+        put(Job.BLAZEWIZARD1, BlazeWizard.MAGIC_GUARD, BlazeWizard.MAGIC_ARMOR);
+        put(Job.BLAZEWIZARD2, BlazeWizard.MEDITATION, BlazeWizard.SPELL_BOOSTER);
+        put(Job.WINDARCHER1, WindArcher.FOCUS);
+        put(Job.WINDARCHER2, WindArcher.BOW_BOOSTER, WindArcher.SOUL_ARROW);
+        put(Job.NIGHTWALKER2, NightWalker.CLAW_BOOSTER, NightWalker.HASTE);
+        put(Job.NIGHTWALKER3, NightWalker.SHADOW_PARTNER);
+        put(Job.THUNDERBREAKER2, ThunderBreaker.KNUCKLER_BOOSTER, ThunderBreaker.LIGHTNING_CHARGE);
+        put(Job.THUNDERBREAKER3, ThunderBreaker.SPEED_INFUSION);
+
+        put(Job.ARAN1, Aran.POLEARM_BOOSTER);
+        put(Job.ARAN2, Aran.BODY_PRESSURE, Aran.COMBO_DRAIN);
+        put(Job.ARAN3, Aran.SNOW_CHARGE, Aran.SMART_KNOCKBACK);
+        put(Job.ARAN4, Aran.MAPLE_WARRIOR, Aran.FREEZE_STANDING, Aran.COMBO_BARRIER);
+
+        put(Job.EVAN3, Evan.MAGIC_GUARD);
+        put(Job.EVAN5, Evan.ELEMENTAL_RESET);
+        put(Job.EVAN6, Evan.MAGIC_SHIELD);
+        put(Job.EVAN7, Evan.MAGIC_BOOSTER);
+        put(Job.EVAN8, Evan.MAGIC_RESISTANCE);
+        put(Job.EVAN10, Evan.MAPLE_WARRIOR);
     }
 
     private BotBuffConfig() {}
