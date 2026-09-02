4c93d6cce4a330b90529b20ff20f6f294283dbb4
Policy <173628847+iampolicy@users.noreply.github.com>
2026-09-02 01:22:46 -0400

    Restore baby bird reactor handler

---
+++ scripts/reactor/1002000.js
@@ -0,0 +1,17 @@
+/*
+This file is part of the OdinMS Maple Story Server
+Copyright (C) 2008 ~ 2010 Patrick Huy <patrick.huy@frz.cc>
+Matthias Butz <matze@odinms.de>
+Jan Christian Meyer <vimes@odinms.de>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License version 3
+as published by the Free Software Foundation. You may not use, modify
+or distribute this program under any other version of the
+GNU Affero General Public License.
+*/
+
+/* Restored legacy handler for reactor action babyBirdItem0. */
+function act() {
+    rm.dropItems();
+}
