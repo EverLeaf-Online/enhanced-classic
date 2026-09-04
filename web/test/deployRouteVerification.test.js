8fa2c07c5e146fba4ed008d16fa0f8be9e676df9
Policy <173628847+iampolicy@users.noreply.github.com>
2026-09-04 01:35:45 -0400

    Update production route markers for data Wiki

--- web/test/deployRouteVerification.test.js
+++ web/test/deployRouteVerification.test.js
@@ -8,9 +8,12 @@ const read=p=>fs.readFileSync(path.join(root,p),'utf8');
 test('public route templates carry production-verifiable markers',()=>{
   const rankings=read('src/views/rankings.ejs');
   const wiki=read('src/views/wiki.ejs');
+  const wikiRoute=read('src/routes/wiki.js');
   assert.match(rankings,/\/character-avatar\/\$\{Number\(r\.id\)\}\.png/);
   assert.match(rankings,/Live saved appearance/);
-  assert.match(wiki,/EVERLEAF PLAYER WIKI/);
-  assert.match(wiki,/Getting Started|getting-started/);
-  assert.match(wiki,/Staff maintained/);
+  assert.match(wiki,/EVERLEAF DATA WIKI/);
+  assert.match(wiki,/WZ \+ MySQL/);
+  assert.match(wiki,/BROWSE CATALOG/);
+  assert.match(wikiRoute,/services\/wikiDataService/);
+  assert.match(wikiRoute,/\/wiki\/guides/);
 });
